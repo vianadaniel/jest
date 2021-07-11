686e352cff03b5e44ff1453fa671e23b217475a5
vianadaniel <daniel@DESKTOP-R4UCBPJ.localdomain>
2021-07-11 18:55:30 -0300

    inicio

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import './app/database/connection';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+app.use(routes);
+
+app.listen(3000, () => console.log('Servidor rodando porta: 3000'));
