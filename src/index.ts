8496441ac453f23c0c933b06f61e5a2f1970b292
vianadaniel <vianadaniel@hotmail.com>
2021-07-11 19:10:26 -0300

    inicio

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,4 @@ app.use(express.json());
 app.use(cors);
 app.use(routes);
 
-app.listen(3000, () => console.log('Servidor rodando porta: 3000'));
+app.listen(3000, () => console.log('**Servidor rodando porta: 3000'));
