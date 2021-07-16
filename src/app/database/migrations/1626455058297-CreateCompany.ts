25ea5643f7c8b977b68696a48192ca535dac0405
Daniel Viana <vianadaniel@hotmail.com>
2021-07-16 14:16:51 -0300

    feat: typeOrm

---
+++ src/app/database/migrations/1626455058297-CreateCompany.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateCompany1626455058297 implements MigrationInterface {
+  name = "CreateCompany1626455058297";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "companies" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "cnpj" character varying(14) NOT NULL, CONSTRAINT "PK_d4bc3e82a314fa9e29f652c2c22" PRIMARY KEY ("id"))`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "companies"`);
+  }
+}
